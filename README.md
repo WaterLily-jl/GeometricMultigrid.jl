d4b4ac6ddb3caba1b3ce8077fd13f00d68ba2c90
Gabriel Weymouth <drweymouth@google.com>
2021-09-13 21:29:05 +0100

    Update benchmark

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Working on high speed solutions for variable coefficient Poisson equations on Ca
 
 | ![mg solver benchmark](benchmark/MGscaling.png) | 
 |:--:| 
-| Median time [benchmark example](benchmark/benchmark.jl) for `mg!` solver demonstrating `NlogN` scaling with the length of the solution vector.|
+| Median time [benchmark example](benchmark/benchmark.jl) for `mg!` solver demonstrating `NlogN` scaling with the length of the solution vector. The dots labeled `psueo` utilize the pseudo-inverse
+smoother designed using AD to acheive approximately 50% speed up with Float32.|
--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -1,19 +1,18 @@
 using LinearAlgebra, GeometricMultigrid, BenchmarkTools, Plots
 
-function setup_2D(n=128)
-    L = zeros(n+2,n+2,2); L[3:n+1,2:n+1,1] .= 1; L[2:n+1,3:n+1,2] .= 1; 
-    A = Poisson(L)
-    x = FieldVec(Float64[i-1 for i ∈ 1:n+2, j ∈ 1:n+2])
-    A,x
+function setup_2D(n=128,T::Type=Float64)
+    L = zeros(T,n+2,n+2,2); L[3:n+1,2:n+1,1] .= 1; L[2:n+1,3:n+1,2] .= 1; 
+    x = T[i-1 for i ∈ 1:n+2, j ∈ 1:n+2]
+    Poisson(L),FieldVec(x)
 end
 
 begin
     suite = BenchmarkGroup()
     nlist = 2 .^ (3:10)
     for n ∈ nlist
-        A,x = setup_2D(n)
-        # suite[n] = @benchmarkable mul!($b,$A,$x)
-        suite[n] = @benchmarkable mg!(st;inner=2) setup=(st=mg_state($A,zero($x),$A*$x))
+        A,x = setup_2D(n,Float32)
+        suite["mg-gs",n] = @benchmarkable mg!(st;inner=2) setup=(st=mg_state($A,zero($x),$A*$x))
+        suite["pseudo",n] = @benchmarkable mg!(st;inner=2) setup=(st=mg_state($A,zero($x),$A*$x,pseudo=true))
     end
 end
 
@@ -22,10 +21,28 @@ results = run(suite)
 begin
     nlogn(n) = n*log(n)
     N = nlist.^2
-    time = [median(results[n]).time for n ∈ nlist]
+    time = [minimum(results["mg-gs",n]).time for n ∈ nlist]
+    ptime = [minimum(results["pseudo",n]).time for n ∈ nlist]
     plot(N,time[end]/N[end] .* N, color=:grey, label="N", legend=:bottomright)
     plot!(N,time[end]/nlogn(N[end]) .* nlogn.(N) , color=:black, label="NlogN")
-    scatter!(N,time, xaxis=("length x",:log10), yaxis=("time (ns)",:log10), label="mg")
+    scatter!(N,time, xaxis=("length x",:log10), yaxis=("time (ns)",:log10), label="mg-GS")
+    scatter!(N,ptime, xaxis=("length x",:log10), yaxis=("time (ns)",:log10), label="mg-pseudo")
 end
 
 savefig("MGscaling.png")
+
+begin
+    A,x = setup_2D()
+    b = zero(x)
+    st = SolveState(A,zero(x),A*x)
+    st.P = PseudoInv(A)
+end
+@btime norm($x)  # 5.3 μs
+@btime dot($b,$x) # 7.0 μs
+@btime dot($b,$A,$x) # 29.5 μs   
+@btime mul!($b,$A,$x) # 30.6 μs
+@btime GeometricMultigrid.increment!($st)  # 45.4 μs
+@btime GeometricMultigrid.GS!($st;inner=1) # 149 μs
+@btime GeometricMultigrid.GS!($st;inner=2) # 238 μs
+@btime GeometricMultigrid.GS!($st)         # 773 μs
+@btime GeometricMultigrid.pseudo!($st)     # 150 μs
